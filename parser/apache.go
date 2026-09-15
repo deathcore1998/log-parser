3a9f24ca7cad2996443f6600e54f8b86b83e8352
Deathcor98 <lesha-11@mail.ru>
2026-09-15 21:45:17 +0300

    Add log line parsing (date, level, client, message)

--- parser/apache.go
+++ parser/apache.go
@@ -1,5 +1,7 @@
 package parser
 
+import "strings"
+
 type LogEntry struct {
 	Data    string
 	Level   string
@@ -8,5 +10,39 @@ type LogEntry struct {
 }
 
 func ParseLine(line string) LogEntry {
-	return LogEntry{}
+
+	logEntry := LogEntry{}
+
+	// find data
+	dataEnd := strings.Index(line, "]") + 1
+	if dataEnd == 0 {
+		return LogEntry{}
+	}
+
+	logEntry.Data = line[:dataEnd]
+	line = strings.TrimSpace(line[dataEnd:])
+
+	if len(line) < 2 || line[0] != '[' {
+		return logEntry
+	}
+
+	// find level
+	levelEnd := strings.Index(line, "]") + 1
+	if levelEnd == 0 {
+		return logEntry
+	}
+
+	logEntry.Level = line[:levelEnd]
+	line = strings.TrimSpace(line[levelEnd:])
+
+	// find client
+	if strings.HasPrefix(line, "[client ") {
+		endClient := strings.Index(line, "]") + 1
+		if endClient != 0 {
+			logEntry.Client = line[:endClient]
+			line = line[endClient:]
+		}
+	}
+	logEntry.Message = line
+	return logEntry
 }
